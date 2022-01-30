cc0d366d62f4bb13b38feadc080aa40712dd9500
kayos@tcp.direct <kayos@tcp.direct>
2022-01-30 03:31:25 -0800

    Improve logging

--- bitslinger.go
+++ bitslinger.go
@@ -58,7 +58,10 @@ func parseUserOpts() {
 
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
-	viper.BindPFlags(pflag.CommandLine)
+	err := viper.BindPFlags(pflag.CommandLine)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse command line arguments")
+	}
 
 	server = viper.GetString("server")
 	proxyURI = viper.GetString("proxy")
@@ -85,37 +88,48 @@ const (
 func reconstructPacket(packetUUID string, packet gopacket.Packet, packetPayload []byte) (buffer gopacket.SerializeBuffer, ok bool) {
 	slog := log.With().Str("caller", packetUUID).Logger()
 
+	defer slog.Trace().Msg("done reconstructing, released!")
+
 	var pp packetProto = unknown
 
+	slog.Trace().Msg("reconstructing packet....")
+
 	// Set flags for TCP vs UDP
-	isTCP := packet.Layer(layers.LayerTypeTCP)
-	isUDP := packet.Layer(layers.LayerTypeUDP)
+	isTCP := packet.Layer(layers.LayerTypeTCP) != nil
+	isUDP := packet.Layer(layers.LayerTypeUDP) != nil
 
 	// Configure Checksums
 	switch {
-	case isTCP != nil:
+	case isTCP:
 		err := packet.TransportLayer().(*layers.TCP).SetNetworkLayerForChecksum(packet.NetworkLayer())
-		if err != nil {
-			slog.Warn().Err(err).Caller().Msg("Failed to set TCP network layer for checksum")
-		} else {
+		if err == nil {
+			slog.Trace().Msg("packet is TCP...")
 			pp = tcp
+			break
 		}
-	case isUDP != nil:
+		slog.Warn().Err(err).Caller().Msg("Failed to set TCP network layer for checksum")
+		break
+	case isUDP:
 		err := packet.TransportLayer().(*layers.UDP).SetNetworkLayerForChecksum(packet.NetworkLayer())
-		if err != nil {
-			slog.Warn().Err(err).Caller().Msg("Failed to set UDP network layer for checksum")
-		} else {
+		if err == nil {
+			slog.Trace().Msg("packet is UDP...")
 			pp = udp
+			break
 		}
+		slog.Warn().Err(err).Caller().Msg("Failed to set UDP network layer for checksum")
+		break
 	default:
-		slog.Debug().Msg("unhandled packet")
+		slog.Trace().Caller().Msg("unhandled packet")
 		return
 	}
 
+	// TODO: Shouldn't be possible? maybe remove
 	if pp == unknown {
 		slog.Debug().Msg("unhandled packet")
 		return
 	}
+
+	slog.Trace().Msg("Setting packet options...")
 
 	// Rebuild with new payload
 	buffer = gopacket.NewSerializeBuffer()
@@ -123,6 +137,8 @@ func reconstructPacket(packetUUID string, packet gopacket.Packet, packetPayload 
 		ComputeChecksums: true,
 		FixLengths:       true,
 	}
+
+	slog.Trace().Msg("Reserializing packet...")
 
 	switch pp {
 	case tcp:
@@ -140,18 +156,24 @@ func reconstructPacket(packetUUID string, packet gopacket.Packet, packetPayload 
 			packet.Layer(layers.LayerTypeUDP).(*layers.UDP),
 			gopacket.Payload(packetPayload),
 		); err != nil {
-			slog.Warn().Err(err).Msg("TCP serialization failure")
+			slog.Warn().Err(err).Msg("UDP serialization failure")
 			return
 		}
+	default:
+		slog.Debug().Msg("unhandled packet")
+		return
 	}
+
 	ok = true
 	return
 }
 
 func releaseFromNfqueue(packetUUID string, packetPayload []byte) {
+	slog := log.With().Str("caller", packetUUID).Logger()
+
 	// Look up nfqueue pointer
 	p, ok := gpq.FromUUID(packetUUID)
-	slog := log.With().Str("caller", packetUUID).Logger()
+
 	if !ok {
 		slog.Debug().Msg("Packet UUID Not found")
 		return
@@ -159,16 +181,17 @@ func releaseFromNfqueue(packetUUID string, packetPayload []byte) {
 
 	// Decode packet from nfqueue
 	packet := gopacket.NewPacket(p.Data(), layers.LayerTypeIPv4, gopacket.Default)
+
 	// Check that packet has a app payload and has been modifed
 	app := packet.ApplicationLayer()
 	if app == nil {
 		// Packet did not have application layer, default accept
-		// slog.Trace().Msg("no application layer")
+		slog.Trace().Msg("no application layer, releasing...")
 		gpq.AcceptAndRelease(packetUUID)
 		return
 	}
 	if testEq(packetPayload, app.Payload()) {
-		// slog.Trace().Msg("packet not modified")
+		slog.Trace().Msg("packet not modified, releasing...")
 		gpq.AcceptAndRelease(packetUUID)
 		return
 	}
@@ -202,6 +225,9 @@ func receivePayloadHTTP(w http.ResponseWriter, req *http.Request) {
 
 func receivePayloadWS(w http.ResponseWriter, r *http.Request) {
 	slog := log.With().Str("caller", r.RemoteAddr).Logger()
+
+	slog.Trace().Msg("websocket payload received...")
+
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		slog.Warn().Err(err).Msg("Failed to upgrade websocket conn")
@@ -214,8 +240,11 @@ func receivePayloadWS(w http.ResponseWriter, r *http.Request) {
 	defer func(c *websocket.Conn) {
 		err := c.Close()
 		if err != nil {
-			slog.Trace().Err(err).Msg("Failed to properly close websocket handler")
+			slog.Debug().Err(err).Msg("Failed to properly close websocket handler")
+		} else {
+			slog.Trace().Msg("done with websocket payload")
 		}
+
 	}(c)
 
 	for {
@@ -225,7 +254,9 @@ func receivePayloadWS(w http.ResponseWriter, r *http.Request) {
 			slog.Warn().Err(err).Msg("read error")
 			break
 		}
-		// log.Println(messageString)
+
+		slog.Trace().Msg(string(message))
+
 		// Segment Message
 		segments := strings.Split(string(message), "\n")
 		if len(segments) < 2 {
@@ -234,36 +265,44 @@ func receivePayloadWS(w http.ResponseWriter, r *http.Request) {
 		}
 
 		slog.Trace().Strs("segments", segments).Msg("websocket message")
+
 		packetUUID := segments[0]
 		payloadHex := segments[1]
 		packetPayload, err := hex.DecodeString(payloadHex)
+
 		if err != nil {
 			slog.Error().Err(err).Interface("payload", payloadHex).Msg("Packet decode failure!")
 			continue
 		}
+
 		releaseFromNfqueue(packetUUID, packetPayload)
 
 	}
 }
 
 func closeResponse(resp *http.Response) {
+	slog := log.With().Str("caller", resp.Request.Header.Get("Packet-Uuid")).Logger()
 	err := resp.Body.Close()
 	if err != nil {
-		log.Debug().Err(err).Msg("failed close response body...")
+		slog.Debug().Err(err).Caller().Msg("failed close response body...")
 	}
+	slog.Trace().Interface("response", resp).Msg("httpModeHandler done")
 }
 
 func httpModeHandler(pckt manager.Packet) {
+	slog := log.With().Str("caller", pckt.UUID()).Logger()
+
 	// HTTP Mode
 	hexEncodedPayload := []byte(hex.EncodeToString(pckt.AppLayer().Payload()))
 	payloadReader := bytes.NewReader(hexEncodedPayload)
 	req, err := http.NewRequest("POST", "http://"+server+"/bitslinger", payloadReader)
 	if err != nil {
-		log.Fatal().Err(err).Msg("failed to submit packet to proxy")
+		slog.Error().Err(err).Msg("failed to craft http request")
+		return
 	}
 	req.Header.Add("Packet-Uuid", pckt.UUID())
+	slog.Trace().Interface("request", req).Msg("Sending HTTP Request")
 	resp, err := httpClient.Do(req)
-
 	defer closeResponse(resp)
 
 	if err == nil {
@@ -276,16 +315,18 @@ func httpModeHandler(pckt manager.Packet) {
 
 func wsModeHandler(pckt manager.Packet) {
 	slog := log.With().Str("caller", pckt.UUID()).Logger()
+
+	defer slog.Trace().Msg("wsModeHandler done")
+
 	hexEncodedPayload := []byte(pckt.UUID() + "\n" + hex.EncodeToString(pckt.AppLayer().Payload()) + "\n")
 	if wsConn != nil {
 		err := wsConn.WriteMessage(websocket.TextMessage, hexEncodedPayload)
 		if err != nil {
 			slog.Warn().Err(err).Msg("WebSocket proxy communication failed, Default forwarding packet as-is")
-
 			gpq.AcceptAndRelease(pckt.UUID())
 		}
 	} else {
-		slog.Warn().Msg("WebSocket proxy communication failed, Default forwarding packet as-is")
+		slog.Warn().Caller().Msg("WebSocket proxy communication failed, Default forwarding packet as-is")
 		pckt.SetVerdict(netfilter.NF_ACCEPT)
 		gpq.Release(pckt.UUID())
 	}
@@ -302,49 +343,56 @@ func sendToProxy(p *netfilter.NFPacket) int {
 		return 0
 	}
 
-	log.Trace().Caller().Str("caller", pckt.UUID()).Msg("-> proxy")
+	log.Trace().Caller().Str("caller", pckt.UUID()).Msg("New Packet")
 
 	switch {
 	case wsMode:
+		log.Trace().Caller().Str("caller", pckt.UUID()).Msg("-> WebSocket")
 		wsModeHandler(pckt)
 	default:
+		log.Trace().Caller().Str("caller", pckt.UUID()).Msg("-> HTTP")
 		httpModeHandler(pckt)
 	}
 	// Needed for C API
 	return 0
 }
 
+func startWebSocketListener() {
+	// WebSocket Listener
+	http.HandleFunc("/bitslinger", receivePayloadWS)
+	log.Info().Msgf("Starting WS listener on: %s\n", "ws://"+server+"/bitslinger")
+	log.Fatal().Err(http.ListenAndServe(server, nil)).Msg("Websocket listen failure")
+}
+
+func startHTTPListener() {
+	// HTTP Listener
+	http.HandleFunc("/bitslinger", receivePayloadHTTP)
+
+	log.Info().Msgf("Starting HTTP listener on: %s\n", "http://"+server+"/bitslinger")
+
+	log.Fatal().Err(http.ListenAndServe(server, nil)).Msg("HTTP listen failure")
+
+	// HTTP Sender
+	proxy, err := url.Parse("http://" + proxyURI)
+	if err != nil {
+		panic(err)
+	}
+	proxyURL = proxy
+	httpClient = &http.Client{
+		Timeout:   0,
+		Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
+	}
+}
+
 func main() {
 	fmt.Println("BitSlinger: The TCP/UDP Packet Payload Editing Tool")
 
 	// Configure Send/Recievers
-	if wsMode {
-		http.HandleFunc("/bitslinger", receivePayloadWS)
-
-		log.Printf("Starting WS listener on: %s\n", "ws://"+server+"/bitslinger")
-		go func() {
-			log.Fatal().Err(http.ListenAndServe(server, nil)).Msg("Websocket listn failure")
-		}()
-
-	} else {
-		// HTTP Listener
-		http.HandleFunc("/bitslinger", receivePayloadHTTP)
-
-		log.Printf("Starting HTTP listener on: %s\n", "http://"+server+"/bitslinger")
-		go func() {
-			log.Fatal().Err(http.ListenAndServe(server, nil)).Msg("HTTP listen failure")
-		}()
-
-		// HTTP Sender
-		proxy, err := url.Parse("http://" + proxyURI)
-		if err != nil {
-			panic(err)
-		}
-		proxyURL = proxy
-		httpClient = &http.Client{
-			Timeout:   0,
-			Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
-		}
+	switch {
+	case wsMode:
+		go startWebSocketListener()
+	default:
+		go startHTTPListener()
 	}
 
 	nfq, err := netfilter.NewNFQueue(uint16(qnum), 1000, netfilter.NF_DEFAULT_PACKET_SIZE)
